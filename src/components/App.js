53489229f0a757175d6e77999233112b55a3e75c
Josh <programmmerjosh@gmail.com>
2022-04-25 16:31:15 +0100

    first few framer-motion animations added

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,27 @@ import ProjectTitles from "./ProjectTitles";
 import downarrow from "../faBrandIcons/chevron-down-solid.svg";
 import ProjectCard from "./ProjectCard";
 import cancel from "../faBrandIcons/ban-solid.svg";
+import { motion } from "framer-motion";
 
 function App() {
+  
+  const emptyProj = {
+    id: "",
+    type: "",
+    source: "",
+    title: "",
+    description: "",
+    imageUrl: "",
+    gitHubUrl: "",
+    directLinkUrl: "",
+    iconSvgs: [],
+  };
   const [category, setCategory] = useState("");
-  const [project, setProject] = useState(null);
+  const [project, setProject] = useState(emptyProj);
   const [id, setId] = useState(null);
 
   function filterProjects(newCategory) {
-    setProject(null);
+    setProject(emptyProj);
     setId(null);
     setCategory(newCategory);
   }
@@ -26,10 +39,34 @@ function App() {
   }
 
   function reset() {
-    setProject(null);
+    setProject(emptyProj);
     setId(null);
     setCategory("");
   }
+
+  const animateInstruction1 = { opacity: category === "" ? 1 : 0 };
+
+  const animateInstruction2 = {
+    opacity: id === null && category !== "" ? 1 : 0,
+  };
+
+  const animateProjectArrow = {
+    opacity: id === null ? 1 : 0,
+    marginTop: id === null ? 0 : -15,
+    position: id === null ? "static" : "fixed",
+  };
+
+  const animateProjectTitles = {
+    opacity: category !== "" ? 1 : 0,
+    marginTop: category !== "" ? 0 : -15,
+    position: category !== "" ? "static" : "fixed",
+  };
+
+  const animateProject = {
+    opacity: id !== null ? 1 : 0,
+    marginTop: id !== null ? 0 : -15,
+    position: id !== null ? "static" : "fixed",
+  };
 
   return (
     <div className="App">
@@ -43,44 +80,46 @@ function App() {
         </button>
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Josh's Projects ~ React Bootstrap Web App</h3>
-        {category === "" && (
+        <motion.div animate={animateInstruction1}>
           <p className="small">
             Tap on any of these categories below to view projects
           </p>
-        )}
+        </motion.div>
+
         <CategoryList
           dataList={myProjects}
           onClick={filterProjects}
           selectedCategory={category}
         />
-        {category !== "" && (
+        <motion.div animate={animateProjectArrow}>
           <img className="faIcon mb-2" src={downarrow} alt="downward arrow" />
-        )}
-        {project === null && category !== "" && (
+        </motion.div>
+
+        <motion.div animate={animateInstruction2}>
           <p className="small">Tap on any of these projects below to view</p>
-        )}
-        {category !== "" && (
+        </motion.div>
+
+        <motion.div animate={animateProjectTitles}>
           <ProjectTitles
             dataList={myProjects.filter((project) => project.type === category)}
             onClick={showDesiredProject}
             selectedId={id}
           />
-        )}
-        {project !== null && (
+        </motion.div>
+
+        <motion.div animate={animateProject}>
           <img className="faIcon mb-2" src={downarrow} alt="downward arrow" />
-        )}
-        <div className="container mb-5">
-          {project !== null && (
-            <ProjectCard
-              description={project.description}
-              imageUrl={project.imageUrl}
-              gitHubUrl={project.gitHubUrl}
-              directLinkUrl={project.directLinkUrl}
-              iconSvgs={project.iconSvgs}
-              source={project.source}
-            />
-          )}
-        </div>
+        </motion.div>
+        <motion.div animate={animateProject} className="container mb-5">
+          <ProjectCard
+            description={project.description}
+            imageUrl={project.imageUrl}
+            gitHubUrl={project.gitHubUrl}
+            directLinkUrl={project.directLinkUrl}
+            iconSvgs={project.iconSvgs}
+            source={project.source}
+          />
+        </motion.div>
       </header>
     </div>
   );
